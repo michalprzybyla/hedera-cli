c8ebeeae756af061f1ca78aeb1dbdd85a3b4fe53
michalrozekariane <michal.rozek@arianelabs.com>
2025-11-05 11:14:37 +0100

    feat: delete duplication for account id regex
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

--- src/core/utils/account-id-validator.ts
+++ src/core/utils/account-id-validator.ts
@@ -1,11 +1,5 @@
-import { z } from 'zod';
-
-const HEDERA_ACCOUNT_ID_REGEX = /^0\.0\.\d+$/;
-
-const accountIdSchema = z.string().regex(HEDERA_ACCOUNT_ID_REGEX, {
-  message: 'Invalid account ID format. Expected: 0.0.123456',
-});
+import { EntityIdSchema } from '../schemas/common-schemas';
 
 export function validateAccountId(accountId: string): void {
-  accountIdSchema.parse(accountId);
+  EntityIdSchema.parse(accountId);
 }
